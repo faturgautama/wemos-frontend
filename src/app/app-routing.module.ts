e4f0b4c477f5f86e0eb5a635052866f475fc1f8c
Fatur Gautama <fatur@paycombat.com>
2024-12-29 13:28:09 +0700

    feat: wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+    // {
+    //     path: '',
+    //     loadComponent: async () => (await import('./pages/landing/lp-home/lp-home.component')).LpHomeComponent
+    // },
+    // {
+    //     path: 'dashboard',
+    //     loadChildren: async () => (await import('./pages/dashboard/dashboard.routes')).dashboardRoutes
+    // }
     {
         path: '',
-        loadComponent: async () => (await import('./pages/landing/lp-home/lp-home.component')).LpHomeComponent
-    },
-    {
-        path: 'dashboard',
-        loadChildren: async () => (await import('./pages/dashboard/dashboard.routes')).dashboardRoutes
+        loadComponent: async () => (await import('./pages/wildcard/wildcard.component')).WildcardComponent
     }
 ];
 
